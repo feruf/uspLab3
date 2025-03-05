33511168d2e43f652c3e89bf5e074aa10731270f
feruf <121869606+feruf@users.noreply.github.com>
2025-03-05 09:48:47 +0200

    Purvi komit

---
+++ feru1/src/bg/tu_varna/sit/1b/qj/Application.kt
@@ -0,0 +1,15 @@
+package bg.tu_varna.sit.`1b`.qj
+
+public class Application {
+    public static void main(String[] args) {
+        Book bookOne = new Book.Builder("123456", "East of Eden", "John Steinbeck")
+            .withGenre("classics")
+            .withPublishingYear(1999)
+            .withPages(512)
+            .withPrice(30.85)
+            .build();
+        Book bookTwo = new Book.Builder("147147", "The Great Gatsby", "Francis Scott Fitzgerald")
+            .withPrice(28.75)
+            .build();
+    }
+}
